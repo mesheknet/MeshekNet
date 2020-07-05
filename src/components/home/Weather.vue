5e25da5b14259c49dc82a14b0f6611bcd206548c
Or Shahar <or1928@gmail.com>
2020-07-05 20:04:36 +0300

    יום א 05 יול 2020 20:04:36

--- src/components/home/Weather.vue
+++ src/components/home/Weather.vue
@@ -6,14 +6,25 @@
           <v-card-title primary-title>
             <h4 class="text--secondary">מזג האויר כרגע באיזור המשק:</h4>
           </v-card-title>
-          <img
-            :src="
-              'http://openweathermap.org/img/wn/' +
-                this.openWeather.list[0].weather[0].icon +
-                '@2x.png'
-            "
-          />
-          <p>{{ this.openWeather.list[0].weather[0].description }}</p>
+          <v-card-text>
+            <h5>{{ this.openWeather.list[0].weather[0].description }}</h5>
+            <v-row align="center">
+              <v-col class="display-3" cols="6">
+                {{ this.openWeather.list[0].main.temp.toFixed(0) }}°c
+              </v-col>
+              <v-col cols="6">
+                <v-img
+                  :src="
+                    'http://openweathermap.org/img/wn/' +
+                      this.openWeather.list[0].weather[0].icon +
+                      '@2x.png'
+                  "
+                  width="92"
+                ></v-img>
+              </v-col>
+            </v-row>
+          </v-card-text>
+
           <v-list-item two-line>
             <v-list-item-content>
               <v-list-item-title>טמפרטורה ממוצעת:</v-list-item-title>
@@ -54,6 +65,36 @@
             </v-list-item-content>
           </v-list-item>
         </v-card>
+        <v-row :align="align" no-gutters style="height: 150px;">
+          <v-col v-for="n in 3" :key="n">
+            <v-card class="mx-auto" max-width="400">
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-title
+                    >מזג האויר כרגע באיזור המשק:</v-list-item-title
+                  >
+                  <v-list-item-subtitle
+                    >Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdiWeatherWindy</v-icon>
+                </v-list-item-icon>
+                <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-cloud-download</v-icon>
+                </v-list-item-icon>
+                <v-list-item-subtitle>48%</v-list-item-subtitle>
+              </v-list-item>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-flex>
     </v-layout>
   </v-container>
