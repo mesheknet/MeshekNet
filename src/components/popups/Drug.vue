50d19279cec0e95bb544d9ae9ab37f0df5fb3d66
lioramir <lioramir.la@gmail.com>
2020-07-25 02:28:24 +0300

    25/07/2020 02:30

--- src/components/popups/Drug.vue
+++ src/components/popups/Drug.vue
@@ -1,134 +1,240 @@
-<template>
+
+ <template>
   <v-dialog max-width="700" v-model="dialog">
     <template v-slot:activator="{ on }">
-      <v-btn
-        block
-        class="mt-6 white--text"
-        color="light green"
-        v-on="on"
-        @click="updateCurrentLogs()"
-      >
-        מחלות<v-icon right>fas fa-history</v-icon>
+      <v-btn block class="mt-6 white--text" color="light green" v-on="on">
+       מחלות<v-icon right>&#x2622;</v-icon>
       </v-btn>
     </template>
 
     <v-card>
       <v-card-title class="green lighten-3" primary-title>
-        מחלות
+       מחלות
       </v-card-title>
 
       <v-card-text>
-        <v-btn text class="ma-2" color="success" @click="addDisease = true"
-          >הוסף מחלה</v-btn
-        >
-        <v-form class="px-3" ref="form" v-if="addDisease">
+        <v-form class="px-3" ref="form">
+        
           <v-row>
             <v-col>
-              <v-text-field label="שם מחלה" v-model="logName"></v-text-field>
+              <v-select
+                v-model="selectedDisease"
+                :items="pests"
+                label="בחר או הוסף מחלה"
+                item-text="name"
+                return-object
+              ></v-select>
             </v-col>
             <v-col>
-              <v-text-field label="נותן התרופה" v-model="actor"></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-menu
-                v-model="dateMenu"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
+              <v-btn
+                @click="addDisease = true"
+                class="ma-2"
+                fab
+                x-small
+                dark
+                color="teal darken-2"
               >
-                <template v-slot:activator="{ on }">
-                  <v-text-field
-                    prepend-icon="date_range"
-                    :value="formattedDate"
-                    label="תאריך ביצוע הפעולה"
-                    readonly
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  locale="he-il"
-                  v-model="logDate"
-                  @input="dateMenu = false"
-                ></v-date-picker>
-              </v-menu>
+                <v-icon dark>add</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn
+                v-if="selectedDisease"
+                @click="deleteDisease"
+                class="ma-2"
+                fab
+                x-small
+                dark
+                color="red darken-2"
+              >
+                <v-icon dark>delete</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row v-if="addDisease">
+            <v-col>
+              <v-text-field label="שם המחלה" v-model="DiseasName"></v-text-field>
             </v-col>
 
             <v-col>
-              <v-text-field label="הערות" v-model="logNotes"></v-text-field>
+              <v-btn text @click="addNewDisease()" class="ma-2" color="success"
+                >הוסף</v-btn
+              >
             </v-col>
           </v-row>
           <v-row>
-            <v-btn
-              text
-              class="ma-2"
-              color="success"
-              @click="
-                addNewAct()
-                addDisease = false
-              "
-              >הוסף</v-btn
-            >
+            <v-col>
+              <v-select
+                v-model="selectedTypeTreatment"
+                :items="pesticides"
+                label="בחר או הוסף סוג טיפול"
+                item-text="name"
+                return-object
+              ></v-select>
+            </v-col>
+            <v-col>
+              <v-btn
+                @click="addTypeTreatment = true"
+                class="ma-2"
+                fab
+                x-small
+                dark
+                color="teal darken-2"
+              >
+                <v-icon dark>add</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn
+                v-if="selectedTypeTreatment"
+                @click="deleteTypeTreatment"
+                class="ma-2"
+                fab
+                x-small
+                dark
+                color="red darken-2"
+              >
+                <v-icon dark>delete</v-icon>
+              </v-btn>
+            </v-col>
           </v-row>
+          <v-row v-if="addTypeTreatment">
+            <v-col>
+              <v-text-field
+                label="סוג טיפול"
+                v-model="TypeTreatmentName"
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              <v-btn
+                text
+                @click="TypeTreatment()"
+                class="ma-2"
+                color="success"
+                >הוסף</v-btn
+              >
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-select
+                v-model="selectedTreatment"
+                :items="pests"
+                label="בחר טיפול או הוסף חדש"
+                item-text="name"
+                return-object
+              ></v-select>
+            </v-col>
+            <v-col>
+              <v-btn
+                @click="addTreatment = true"
+                class="ma-2"
+                fab
+                x-small
+                dark
+                color="teal darken-2"
+              >
+                <v-icon dark>add</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn
+                v-if="selectedTreatment"
+                @click="deleteTreatment"
+                class="ma-2"
+                fab
+                x-small
+                dark
+                color="red darken-2"
+              >
+                <v-icon dark>delete</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row v-if="addTreatment">
+            <v-col>
+              <v-text-field label="תאור הטיפול" v-model="TreatmentName"></v-text-field>
+            </v-col>
+
+            <v-col>
+              <v-btn text @click="addNewTreatment()" class="ma-2" color="success"
+                >הוסף</v-btn
+              >
+            </v-col>
+          </v-row>
+            <v-row>
+            <v-col>
+              <v-select
+                v-model="selectedDrug"
+                :items="pests"
+                label="בחר או הוסף תרופה"
+                item-text="name"
+                return-object
+              ></v-select>
+            </v-col>
+            <v-col>
+              <v-btn
+                @click="addDrug = true"
+                class="ma-2"
+                fab
+                x-small
+                dark
+                color="teal darken-2"
+              >
+                <v-icon dark>add</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn
+                v-if="selectedDrug"
+                @click="deleteDrug"
+                class="ma-2"
+                fab
+                x-small
+                dark
+                color="red darken-2"
+              >
+                <v-icon dark>delete</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row v-if="addDrug">
+            <v-col>
+              <v-text-field label="שם התרופה" v-model="DrugName"></v-text-field>
+            </v-col>
+
+            <v-col>
+              <v-btn text @click="addNewDrug()" class="ma-2" color="success"
+                >הוסף</v-btn
+              >
+            </v-col>
+          </v-row>
+
+         
         </v-form>
-
-        <v-divider></v-divider>
-        <v-text-field
-          v-model="search"
-          append-icon="search"
-          label="חיפוש"
-          single-line
-          hide-details
-          class="mb-4"
-        ></v-text-field>
-
-        <v-data-table
-          :search="search"
-          :headers="headers"
-          :items="cropLog"
-          :page.sync="page"
-          :items-per-page="itemsPerPage"
-          hide-default-footer
-          class="elevation-1"
-          @page-count="pageCount = $event"
-        >
-          <template slot="no-data">
-            <p class="text-right  ">
-              עדיין לא ניתנו תרופות למזור תרנגולות זה
-            </p>
-          </template>
-
-          <template v-slot:item.actions="{ item }">
-            <v-icon small @click="deleteLog(item)">
-              delete
-            </v-icon>
-          </template>
-        </v-data-table>
-        <div class="text-center pt-2">
-          <v-pagination
-            :next-icon="nextIcon"
-            :prev-icon="prevIcon"
-            v-model="page"
-            :length="pageCount"
-            color="teal darken-3"
-          ></v-pagination>
-        </div>
       </v-card-text>
 
       <v-divider></v-divider>
 
       <v-card-actions>
-        <v-btn block @click="dialog = false" color="success">חזרה</v-btn>
+        <v-btn
+          :loading="loading"
+          block
+          @click="
+            addPesticide = false
+            addPest = false
+            addPimplement()
+          "
+          color="success"
+          >הוסף מחלה </v-btn
+        >
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-import moment from 'moment'
+
 const fb = require('@/fb.js')
 import { mapGetters } from 'vuex'
 
@@ -137,64 +243,79 @@ export default {
   components: {},
   data() {
     return {
+      loading: false,
       dialog: null,
-      headers: [
-        {
-          text: 'שם התרופה',
-          align: 'start',
-          value: 'DrugName'
-        },
-        { text: 'נותן התרופה', value: 'actor' },
-        { text: 'תאריך נתינה', value: 'logDate' },
-        { text: 'הערות', value: 'logNotes' },
-        { text: 'מחיקה', value: 'actions' }
-      ],
-      search: '',
-      page: 1,
-      pageCount: 0,
-      itemsPerPage: 5,
-      nextIcon: 'navigate_next',
-      prevIcon: 'navigate_before',
-      logName: null,
-      actor: null,
-      logDate: new Date().toISOString().substr(0, 10),
-      logNotes: null,
-      dateMenu: false,
-      logList: [],
-      addDisease: false
+      addDisease: false,
+      addTypeTreatment: false,
+      addTreatment: false,
+      addDrug: false,
+      selectedDisease: null,
+      DiseasName: null,
+      selectedTypeTreatment: null,
+      TypeTreatmentName: null,
+      selectedTreatment: null,
+      TreatmentName: null,
+      selectedDrug: null,
+      DrugName: null,
+      dosage: null,
+      vol: null
     }
   },
   methods: {
-    addNewAct() {
-      fb.cropLog
-        .doc()
+    addNewPesticide() {
+      let ref = fb.pesticide.doc()
+      ref
         .set({
-          cycleId: this.currentCycle.id,
-          logName: this.logName,
-          actor: this.actor,
-          logDate: moment(this.logDate).format('L'),
-          logNotes: this.logNotes
+          name: this.pesticideName,
+          supplier: this.supplierName,
+          id: ref.id
         })
-        //reset form values
-        .then(this.$refs.form.reset())
-    },
-    //get current cycle logs using store
-    updateCurrentLogs() {
-      this.$store.dispatch('bindCropLog')
+        .then(
+          (this.selectedPesticide = this.pesticides.find(
+            obj => obj.id == ref.id
+          ))
+        )
+        .then((this.addPesticide = false))
     },
 
-    deleteLog(item) {
-      fb.cropLog.doc(item.id).delete()
+    addNewPest() {
+      let ref = fb.pest.doc()
+      ref
+        .set({ name: this.pestName, id: ref.id })
+        .then((this.selectedPest = this.pests.find(obj => obj.id == ref.id)))
+        .then((this.addPest = false))
+    },
+    addPimplement() {
+      this.loading = true
+      let ref = fb.pImplement.doc()
+      ref
+        .set({
+          cropId: this.selectedCrop.id,
+          pestId: this.selectedPest.id,
+          pestName: this.selectedPest.name,
+          pesticideId: this.selectedPesticide.id,
+          pesticideName: this.selectedPesticide.name,
+          pesticideSupplier: this.selectedPesticide.supplier,
+          dosage: this.dosage,
+          vol: this.vol
+        })
+        .then(
+          (this.loading = false),
+          (this.dialog = false),
+          this.$refs.form.reset()
+        )
+    },
+    deletePest() {
+      fb.pest.doc(this.selectedPest.id).delete()
+    },
+    deletePesticide() {
+      fb.pesticide.doc(this.selectedPesticide.id).delete()
     }
   },
-  created() {},
+  updated() {},
   computed: {
     //get local data from firestore using the store
-    ...mapGetters(['farmId', 'currentCycle', 'cropLog', 'LOADED']),
-
-    formattedDate() {
-      return this.logDate ? moment(this.logDate).format('L') : ''
-    }
+    ...mapGetters(['pests', 'pesticides', 'pImplement', 'crops'])
   }
 }
 </script>
