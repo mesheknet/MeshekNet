cd655a6e7c450885119a6bfeca6f1565d56a060b
Or Shahar <or1928@gmail.com>
2020-08-17 20:20:00 +0300

    יום ב 17 אוג 2020 20:20:00

--- src/components/admin/AddCropData.vue
+++ src/components/admin/AddCropData.vue
@@ -26,6 +26,7 @@
               <v-text-field
                 v-model="cropDuration"
                 label="משך המחזור"
+                suffix="שבועות"
               ></v-text-field>
             </v-col>
           </v-row>
@@ -63,7 +64,34 @@
           </v-row>
           <v-row>
             <v-col>
-              <v-text-field v-model="irrigation" label="השקיה"></v-text-field>
+              <v-text-field
+                v-model="irrigation13"
+                label="השקיה - תבור-כדורי"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="irrigation28"
+                label="השקיה - ניר העמק עפולה"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="irrigation48"
+                label="השקיה - בית הערבה"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="irrigation54"
+                label="השקיה - בית דגן"
+              ></v-text-field>
             </v-col>
           </v-row>
           <v-row>
@@ -87,7 +115,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
-//const fb = require('@/fb.js')
+const fb = require('@/fb.js')
 
 export default {
   name: 'AddCropData',
@@ -98,7 +126,10 @@ export default {
       dialog: null,
       cropName: null,
       cropDuration: null,
-      irrigation: [],
+      irrigation13: [],
+      irrigation28: [],
+      irrigation48: [],
+      irrigation54: [],
       fertNitrogen: [],
       fertPhosphorus: [],
       fertPotassium: [],
@@ -108,28 +139,51 @@ export default {
   methods: {
     stringToarr(sourceArr) {
       if (sourceArr == []) {
-        return null
+        return []
       } else {
         sourceArr = sourceArr.split(' ')
-        let fertArr = []
+        let floatArr = []
         sourceArr.forEach((item) => {
-          fertArr.push(parseFloat(item))
+          floatArr.push(parseFloat(item))
         })
-        return fertArr
+        return floatArr
       }
     },
 
     addCropData() {
-      this.getFertArrays()
-      console.log(this.fertPhosphorus)
+      this.getFloatArrays()
+      let fertilizationMap = {
+        Nitrogen: this.fertNitrogen,
+        Phosphorus: this.fertPhosphorus,
+        Potassium: this.fertPotassium,
+        Urean: this.fertUrean,
+      }
+      let irrigationMap = {
+        13: this.irrigation13,
+        28: this.irrigation28,
+        48: this.irrigation48,
+        54: this.irrigation54,
+      }
+      let ref = fb.crop.doc()
+      ref.set({
+        id: ref.id,
+        duration: this.cropDuration,
+        name: this.cropName,
+        fertilization: fertilizationMap,
+        irrigation: irrigationMap,
+      })
     },
 
     //transform user input into array of float numbers
-    getFertArrays() {
+    getFloatArrays() {
       this.fertNitrogen = this.stringToarr(this.fertNitrogen)
       this.fertPhosphorus = this.stringToarr(this.fertPhosphorus)
       this.fertPotassium = this.stringToarr(this.fertPotassium)
       this.fertUrean = this.stringToarr(this.fertUrean)
+      this.irrigation13 = this.stringToarr(this.irrigation13)
+      this.irrigation28 = this.stringToarr(this.irrigation28)
+      this.irrigation48 = this.stringToarr(this.irrigation48)
+      this.irrigation54 = this.stringToarr(this.irrigation54)
     },
   },
   updated() {},
