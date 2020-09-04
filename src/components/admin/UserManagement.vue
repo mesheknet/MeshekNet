e68d5c15f9d73230e290b06758ed41d4e70b2ba3
Or Shahar <or1928@gmail.com>
2020-09-04 13:21:58 +0300

    יום ו 04 ספט 2020 13:21:58

--- src/components/admin/UserManagement.vue
+++ src/components/admin/UserManagement.vue
@@ -15,48 +15,37 @@
       <v-card-title class="green lighten-3" primary-title>
         ניהול משתמשים
       </v-card-title>
-<v-container>
-   <v-form ref="form" v-model="valid" lazy-validation>
-      <v-row>
-        
-          <v-select
-            v-model="selectUser"
-            :items="farmOwners"
-            label="בחר משתמש"
-            item-text="name"
-            return-object
-          ></v-select>
-        </v-row>
-        <v-row>
-         <v-btn
-                
-                color="success"
-                class="mr-4"
-                @click="BecomeAdmin"
-                
-              >
-                הפוך למנהל
-              </v-btn>
-             
-         <v-btn
-                
-                
-                class="mr-4"
-                @click="CanceledAdmin"
-                
-              >
-                בטל מנהל
-              </v-btn>
-              </v-row>
-              <v-row>
-              <v-btn color="error" class="mr-4" @click="dialog = false">
-                חזור
-              </v-btn>
-              </v-row>
-              </v-form>
-     </v-container>
+      <v-container>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-row>
+            <v-select
+              v-model="selectUser"
+              :items="farmOwners"
+              label="בחר משתמש"
+              item-text="name"
+              return-object
+            ></v-select>
+          </v-row>
+          <v-row>
+            <v-btn color="success" class="mr-4" @click="BecomeAdmin">
+              הפוך למנהל
+            </v-btn>
+
+            <v-btn class="mr-4" @click="CanceledAdmin">
+              בטל מנהל
+            </v-btn>
+          </v-row>
+          <v-row>
+            <v-btn color="error" class="mr-4" @click="dialog = false">
+              חזור
+            </v-btn>
+          </v-row>
+          <v-snackbar v-model="snackbar">
+            הפעולה בוצעה.
+          </v-snackbar>
+        </v-form>
+      </v-container>
     </v-card>
-    
   </v-dialog>
 </template>
 
@@ -69,40 +58,38 @@ export default {
   components: {},
   data() {
     return {
+      snackbar: false,
       loading: false,
       dialog: null,
-      selectUser:null
-    
+      selectUser: null,
     }
   },
   methods: {
     BecomeAdmin() {
-        //  fb.user.doc(this.selectUser.userId).update({
-     //   admin: true,
-      //})
-      for(var i=0;i<this.users.length;i++)
-   if(this.users[i].userId==this.selectUser.userId){
-    fb.user.doc(this.users[i].email).update({
+      let uEmail = this.users.find(
+        (item) => item.userId == this.selectUser.userId
+      ).email
+      fb.user.doc(uEmail).update({
         admin: true,
       })
+      this.snackbar = true
       this.$refs.form.reset()
-   }
     },
- CanceledAdmin() {
-   for(var i=0;i<this.users.length;i++)
-   if(this.users[i].userId==this.selectUser.userId){
-    fb.user.doc(this.users[i].email).update({
+    CanceledAdmin() {
+      let uEmail = this.users.find(
+        (item) => item.userId == this.selectUser.userId
+      ).email
+      fb.user.doc(uEmail).update({
         admin: false,
       })
+      this.snackbar = true
       this.$refs.form.reset()
-   }
     },
-    
   },
   updated() {},
   computed: {
     //get local data from firestore using the store
-    ...mapGetters(['farmOwners','users']),
+    ...mapGetters(['farmOwners', 'users']),
   },
 }
 </script>
